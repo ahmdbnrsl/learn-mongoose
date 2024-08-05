a855dc60a99472df2559aaf1870e66ef6c1ae4fa
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-05 14:38:10 +0700

    inserting data

--- test/001-basic.test.ts
+++ test/001-basic.test.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, Document, Model } from 'mongoose';
 import 'dotenv/config';
 
 const URI: string = process.env.MONGO_URI || '';
@@ -9,6 +9,36 @@ describe('BASIC', () => {
             await mongoose
                 .connect(URI)
                 .then(() => console.log('success connecting'));
+        } catch (error) {
+            console.error('Error');
+        } finally {
+            await mongoose.connection.close();
+        }
+    });
+
+    it('should insert data to mongodb', async () => {
+        try {
+            await mongoose.connect(URI);
+            interface Persons extends Document {
+                name: string;
+                age: string;
+            }
+            const PersonSchema: Schema<Persons> = new Schema({
+                name: {
+                    type: String,
+                    required: true
+                },
+                age: {
+                    type: String,
+                    required: true
+                }
+            });
+            const Person = mongoose.model('Person', PersonSchema);
+            const person = new Person({
+                name: 'Ahmad Beni Rusli',
+                age: '19'
+            });
+            await person.save().then((person: any) => console.info(person));
         } catch (error) {
             console.error('Error');
         } finally {
